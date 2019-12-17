8da1020a5ff203dcc668ff69a4d388c9db263674
faqima01 <faqima01@luther.edu>
2019-12-17 00:37:06 -0600

    Update routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Auth0Provider } from './react-auth0-spa';
 import config from "./auth_config.json";
 
@@ -16,14 +17,16 @@ const onRedirectCallback = appState => {
 };
 
 ReactDOM.render(
-    <Auth0Provider
-        domain={config.domain}
-        client_id={config.clientId}
-        redirect_uri={window.location.origin}
-        onRedirectCallback={onRedirectCallback}
-    >
-        <App />
-    </Auth0Provider>,
+    <Router>
+        <Auth0Provider
+            domain={config.domain}
+            client_id={config.clientId}
+            redirect_uri={window.location.origin}
+            onRedirectCallback={onRedirectCallback}
+        >
+            <App />
+        </Auth0Provider>
+    </Router>,
     document.getElementById('root')
 );
 
