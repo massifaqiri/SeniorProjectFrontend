ef9ad15d844af4cfe9b8e2e8afc05662b90566c0
karinaehoff <hoffka04@luther.edu>
2020-04-05 13:01:16 -0500

    Initial Commit

---
+++ tests/dom.js
@@ -0,0 +1,25 @@
+import { JSDOM } from 'jsdom';
+
+const { window } = new JSDOM('<!doctype html><html><body></body></html>');
+
+if (typeof define !== 'function') {
+    global.define = require('amdefine')(module);
+}
+
+function copyProps(src, target) {
+    const props = Object.getOwnPropertyNames(src)
+        .filter(prop => typeof target[prop] === 'undefined')
+        .reduce((result, prop) => ({
+            ...result,
+            [prop]: Object.getOwnPropertyDescriptor(src, prop),
+        }), {});
+    Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.decument = window.document;
+global.navigator = {
+    userAgent: 'node.js',
+};
+
+copyProps(window, global);
